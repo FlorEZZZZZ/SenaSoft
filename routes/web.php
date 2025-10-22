22e50ed8aa5b6923b13a58fff4c87bb3f2ca91bb
Cami050 <102600863+Cami050@users.noreply.github.com>
2025-10-22 14:51:25 -0500

    Ajustes

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,23 @@ Route::get('/', function () {
 Route::get('/search', function () {
     return view('flights.search');
 });
+
+Route::get('/search-results', function () {
+    return view('flights.search-results');
+});
+
+Route::get('/bookings/create', function () {
+    return view('bookings.create');
+});
+
+Route::get('/bookings/payment', function () {
+    return view('bookings.payment');
+});
+
+Route::get('/bookings/confirmation', function () {
+    return view('bookings.confirmation');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
